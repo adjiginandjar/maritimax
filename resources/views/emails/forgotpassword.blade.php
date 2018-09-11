2935b8d0640dcca0fe89375cf022650e3693bbe3
marfycarlo <marfycarlo@github.com>
2018-09-12 02:05:07 +0700

    forgot password tpl

--- resources/views/emails/forgotpassword.blade.php
+++ resources/views/emails/forgotpassword.blade.php
@@ -1,5 +1,10 @@
 Hi <strong>{{ $name }}</strong>,
+<br><br>
+We have received a reqeust to reset your password, If you didn't make the request,<br>
+just ignore this email. Otherwise, you can reset your password using this link : <br><br>
+<a style="background-color:#0577cb; text-decoration:none; font-size:1.2em; font-weight:700; color:#ffffff; text-align:center; padding:1em" href={{ URL::to('http://maritimax.com/reset/password?token=' . $token) }}> Click here to Reset Password </a>
+<br><br>
+Regards,<br>
+Maritimax Team
 
-<p>{{ $body }}</p>
 
-<a href={{ URL::to('http://siapayangnanya.com/reset/password?token=' . $token) }}> link </a>
