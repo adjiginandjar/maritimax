a52cf3b6ba7cdf5b7e4a35f9eeed2000c134556f
marfycarlo <marfycarlo@github.com>
2018-09-12 02:09:30 +0700

    more enter

--- resources/views/emails/forgotpassword.blade.php
+++ resources/views/emails/forgotpassword.blade.php
@@ -1,8 +1,9 @@
 Hi <strong>{{ $name }}</strong>,
 <br><br>
-We have received a reqeust to reset your password, If you didn't make the request,<br>
-just ignore this email. Otherwise, you can reset your password using this link : <br><br>
-<a style="background-color:#0577cb; text-decoration:none; font-size:1.2em; font-weight:700; color:#ffffff; text-align:center; padding:1em" href={{ URL::to('http://maritimax.com/reset/password?token=' . $token) }}> Click here to Reset Password </a>
+We have received a request to reset your password, If you didn't make the request,<br>
+just ignore this email. Otherwise, you can reset your password using this link : <br><br><br><br>
+<a style="background-color:#0577cb; text-decoration:none; font-size:1.1em; font-weight:700; color:#ffffff; text-align:center; padding:1em 2em" href={{ URL::to('http://maritimax.com/reset/password?token=' . $token) }}> Click here to Reset Password</a>
+<br><br>
 <br><br>
 Regards,<br>
 Maritimax Team
